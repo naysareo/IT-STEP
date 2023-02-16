17833ff0e712a6399f0243c1f3d35afd229cefdd
Arseny <33712421+naysareo@users.noreply.github.com>
2023-02-16 20:12:08 +0300

    Update and rename txt to NEW Class.py

---
+++ NEW Class.py
@@ -0,0 +1,22 @@
+# 16.02 H/W
+#Перевернуть строку 1000000001 
+# Работа через цикл
+
+
+#############################
+val_str = 'string'
+some_list = []
+some_list.extend('string')
+i = some_list.index('i')
+some_list[3] = 'O'
+print(some_list)
+
+# или
+
+val_str = 'string'
+some_list = []
+some_list.extend('string')
+i = some_list.index('i')
+some_list[i] = 'O'
+print(some_list)
+#############################
